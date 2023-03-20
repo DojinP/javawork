0fa9e5a125551a38b621435b581eef21e271d4ee
DojinP <coah2013@naver.com>
2023-03-20 13:07:08 +0900

    2023/03/20 수업 커밋 - 2  - Single Chatting Program (Thread 사용)  - .gitignore 수정

--- advancedJava/src/network/SingleChatClient.java
+++ advancedJava/src/network/SingleChatClient.java
@@ -11,22 +11,26 @@ public class SingleChatClient {
 
 	public static void main(String[] args) {
 		Socket sock = null;
-		
-		
 
 		try {
 			// 초기화
+			// Socket 객체를 생성하면 바로 서버에 접속된다.
+			// 서버 측에서는 accept 메소드가 실행된다.
 			sock = new Socket("172.30.1.41", 12345);
 			System.out.println("연결되었습니다.");
 			
-			ClientSenderThread send = new ClientSenderThread(sock);
-			ClientReceiverThread recv = new ClientReceiverThread(sock);
+			// 서버와 통신할 쓰레드 객체를 생성해서 start 하기
+//			ClientSenderThread send = new ClientSenderThread(sock);
+//			ClientReceiverThread recv = new ClientReceiverThread(sock);
 			
-			Thread sender = new Thread(send, "send");
-			Thread receiver = new Thread(recv, "recv");
+//			Thread sender = new Thread(send, "send");
+//			Thread receiver = new Thread(recv, "recv");
 			
-			sender.start();
-			receiver.start();
+//			sender.start();
+//			receiver.start();
+			
+			new Thread(new ClientSenderThread(sock)).start();
+			new Thread(new ClientReceiverThread(sock)).start();
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
--- advancedJava/src/network/SingleChatServer.java
+++ advancedJava/src/network/SingleChatServer.java
@@ -16,14 +16,20 @@ public class SingleChatServer {
 		try {
 			server = new ServerSocket(12345);
 			System.out.println("클라이언트 대기중...");
-			sock = server.accept();
-			System.out.println(sock.getInetAddress().getHostAddress() + " 호스트가 접속하였습니다.");
 			
-			ServerSenderThread send = new ServerSenderThread(sock);
-			ServerReceiverThread recv = new ServerReceiverThread(sock);
-			
-			send.start();
-			recv.start();
+			while (true) {
+				sock = server.accept();
+				System.out.println(sock.getInetAddress().getHostAddress() + " 호스트가 접속하였습니다.");
+				// 클라이언트와 통신하기 위한 쓰레드 두 개를 생성해서 start
+				//					  ---------
+				//						ㄴ> input/output에 대한 처리
+				
+				ServerSenderThread send = new ServerSenderThread(sock);
+				ServerReceiverThread recv = new ServerReceiverThread(sock);
+				
+				send.start();
+				recv.start();
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
